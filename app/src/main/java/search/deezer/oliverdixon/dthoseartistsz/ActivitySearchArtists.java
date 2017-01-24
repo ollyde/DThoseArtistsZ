cd56a3cbac817cf61f73744a1f56e72f56898181
Oliver Dixon <olly257@gmail.com>
2017-01-24 21:29:30 +0100

    [JIRA-NUMBER] : First commit, initial setup.

---
+++ app/src/main/java/search/deezer/oliverdixon/dthoseartistsz/ActivitySearchArtists.java
@@ -0,0 +1,14 @@
+package search.deezer.oliverdixon.dthoseartistsz;
+
+import android.os.Bundle;
+
+import search.deezer.oliverdixon.dthoseartistsz.common.BaseActivity;
+
+public class ActivitySearchArtists extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_artists);
+    }
+}

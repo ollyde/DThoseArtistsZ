3354927d5e10a2deafcf0da3e06249b0054987ce
Oliver Dixon <olly257@gmail.com>
2017-01-28 18:12:41 +0100

    Began work on albums activtiy.

---
+++ app/src/main/java/search/deezer/oliverdixon/dthoseartistsz/ActivityAlbumInfo.java
@@ -0,0 +1,16 @@
+package search.deezer.oliverdixon.dthoseartistsz;
+
+import android.os.Bundle;
+
+import search.deezer.oliverdixon.dthoseartistsz.common.BaseActivity;
+
+public class ActivityAlbumInfo extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_info);
+
+    }
+
+}
